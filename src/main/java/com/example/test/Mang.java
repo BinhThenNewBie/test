7b872f69b9ca3020a84e748a8ae7158e379db1b1
BinhLearnToCode <binhdath06811@gmail.com>
2025-12-13 09:07:24 +0700

    check

--- src/main/java/com/example/test/Mang.java
+++
@@ -1,8 +0,0 @@
-package com.example.test;
-
-public class Mang {
-    public int GetElementAtIndex (int[] array, int index) {
-        //java tự trả lại outofbounds nếu index không hợp lệ
-        return array[index];
-}
-}
--- src/main/java/com/example/test/ProfileManager.java
+++
@@ -1,7 +0,0 @@
-package com.example.test;
-
-public class ProfileManager {
-    public String getName(Profile profile) {
-        return profile.getName();
-    }
-}
